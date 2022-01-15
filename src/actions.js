51e360bf2dda93fff533ad89f259d2faf8b75aeb
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-01-15 23:29:02 +0500

    destructing-code(add files: actions, reducer)

---
+++ src/actions.js
@@ -0,0 +1,8 @@
+export const inc = () => ({type: "INC"})
+
+export const dec = () => ({type: "DEC"})
+
+export const rnd = (random) => ({
+    type: "RND",
+    random
+})
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
+import { createStore } from "redux";
+import reducer from "./reducer";
+import {dec, inc, rnd} from './actions'
 
-const reducer = (state = 0, action) => { 
-    // state = 0
-    // remove "if else" operator 
-    // if (state === undefined) {
-    //     return 0;
-    // }
-    switch (action.type) {
-        case 'INC':
-            return state + 1
-        default:
-            return state
-    }
-}
- 
-let state = reducer(undefined, {})
+const store = createStore(reducer);
 
-state = reducer(state, {type: 'INC'})
-console.log(state);
 
-state = reducer(state, {type: 'INC'})
-console.log(state);
+document
+.getElementById('inc')
+.addEventListener('click', () => {
+    store.dispatch(inc())
+})
+
+document
+.getElementById('dec')
+.addEventListener('click', () => {
+    store.dispatch(dec())
+})
+
+document
+.getElementById('rnd')
+.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * 10)
+    store.dispatch(rnd(random))
+})
+
+store.subscribe(() => {
+    document
+    .getElementById('counter')
+    .innerHTML = store.getState()
+})
